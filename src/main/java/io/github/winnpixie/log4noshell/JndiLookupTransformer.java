adc5943fcb6155a7f170c77379cb73024aa6b9fa
Hannah <salvadin2@gmail.com>
2025-04-27 10:06:57 -0400

    remove asm:tree dep

--- src/main/java/io/github/winnpixie/log4noshell/JndiLookupTransformer.java
+++ src/main/java/io/github/winnpixie/log4noshell/JndiLookupTransformer.java
@@ -1,46 +1,39 @@
 package io.github.winnpixie.log4noshell;
 
 import org.objectweb.asm.*;
-import org.objectweb.asm.tree.ClassNode;
-import org.objectweb.asm.tree.InsnNode;
-import org.objectweb.asm.tree.MethodNode;
 
 import java.lang.instrument.ClassFileTransformer;
-import java.lang.instrument.IllegalClassFormatException;
 import java.security.ProtectionDomain;
 
 public class JndiLookupTransformer implements ClassFileTransformer {
-    private static final String LOOKUP_CLASS_NAME = "org/apache/logging/log4j/core/lookup/JndiLookup";
-    private static final String LOOKUP_METHOD_NAME = "lookup";
-    private static final String LOOKUP_METHOD_DESC = "(Lorg/apache/logging/log4j/core/LogEvent;Ljava/lang/String;)Ljava/lang/String;";
+    private static final String CLASS_NAME = "org/apache/logging/log4j/core/lookup/JndiLookup";
+    private static final String METHOD_NAME = "lookup";
+    private static final String METHOD_DESC = "(Lorg/apache/logging/log4j/core/LogEvent;Ljava/lang/String;)Ljava/lang/String;";
 
     @Override
     public byte[] transform(ClassLoader loader, String className, Class<?> classBeingRedefined,
-                            ProtectionDomain protectionDomain, byte[] classfileBuffer) throws IllegalClassFormatException {
-        if (!LOOKUP_CLASS_NAME.equals(className)) return null;
+                            ProtectionDomain protectionDomain, byte[] classfileBuffer) {
+        if (!className.equals(CLASS_NAME)) return null;
 
-        try {
-            ClassReader classReader = new ClassReader(classfileBuffer);
-            ClassNode classNode = new ClassNode();
-            classReader.accept(classNode, ClassReader.SKIP_FRAMES);
+        ClassReader cr = new ClassReader(classfileBuffer);
+        ClassWriter cw = new ClassWriter(cr, ClassWriter.COMPUTE_FRAMES);
+        ClassVisitor cv = new ClassVisitor(Opcodes.ASM9, cw) {
+            @Override
+            public MethodVisitor visitMethod(int access, String name, String descriptor, String signature, String[] exceptions) {
+                if (!name.equals(METHOD_NAME)) return null;
+                if (!descriptor.equals(METHOD_DESC)) return null;
 
-            for (MethodNode methodNode : classNode.methods) {
-                if (!methodNode.name.equals(LOOKUP_METHOD_NAME)) continue;
-                if (!methodNode.desc.equals(LOOKUP_METHOD_DESC)) continue;
-
-                methodNode.instructions.clear();
-                methodNode.instructions.add(new InsnNode(Opcodes.ACONST_NULL));
-                methodNode.instructions.add(new InsnNode(Opcodes.ARETURN));
+                return new MethodVisitor(Opcodes.ASM9, cw.visitMethod(access, name, descriptor, signature, exceptions)) {
+                    @Override
+                    public void visitCode() {
+                        super.visitInsn(Opcodes.ACONST_NULL);
+                        super.visitInsn(Opcodes.ARETURN);
+                    }
+                };
             }
+        };
 
-            ClassWriter classWriter = new ClassWriter(classReader, ClassWriter.COMPUTE_FRAMES);
-            classNode.accept(classWriter);
-
-            System.out.println("Attempting to patch JndiLookup#lookup");
-            return classWriter.toByteArray();
-        } catch (ClassTooLargeException | MethodTooLargeException e) {
-            e.printStackTrace();
-            return null;
-        }
+        cr.accept(cv, ClassReader.SKIP_FRAMES);
+        return cw.toByteArray();
     }
 }
--- src/main/java/io/github/winnpixie/log4noshell/Log4NoShellAgent.java
+++ src/main/java/io/github/winnpixie/log4noshell/Log4NoShellAgent.java
@@ -3,10 +3,10 @@ package io.github.winnpixie.log4noshell;
 import java.lang.instrument.Instrumentation;
 
 public class Log4NoShellAgent {
-    public static final String VERSION = "0.7";
+    public static final String VERSION = "0.8";
 
     public static void premain(String args, Instrumentation inst) {
-        System.out.printf("Log4NoShell %s, a patch for CVE-2021-44228 (Log4Shell).%n", VERSION);
+        System.out.printf("Log4NoShell %s, patches CVE-2021-44228 (Log4Shell)%n", VERSION);
 
         inst.addTransformer(new JndiLookupTransformer());
     }
